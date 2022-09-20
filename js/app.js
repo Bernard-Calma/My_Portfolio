992504b92e6dd8a456bb9d20085583a49ead8f49
nard.calma@gmail.com <nard.calma@gmail.com>
2022-09-20 01:20:31 -0700

    added effects

--- js/app.js
+++ js/app.js
@@ -5,9 +5,27 @@ const openMenu = (e, className) => {
     // console.log(content[0].attributes)
     for( let i = 0; i < content.length; i++){
         content[i].setAttribute("hidden", true)
+        content[i].className = content[i].className + " slideUp"
     }
 
     let showContent
     showContent = document.getElementById(className)
     showContent.removeAttribute("hidden")
+}
+
+document.body.style.height = window.height
+footer = document.querySelector("footer")
+// console.log(footer.getBoundingClientRect().top)
+
+
+window.onscroll = () => {
+    // console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 350) {
+        footer.style.color = "white";
+        footer.className = "slideUp";
+        // console.log(footer.getBoundingClientRect())
+    } else {
+        footer.style.color = "black";
+        footer.className = "slideDown";
+    }
 }
